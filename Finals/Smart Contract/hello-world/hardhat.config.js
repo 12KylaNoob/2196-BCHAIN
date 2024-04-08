e87ad70b56abe02e19136b5ba41fc9681eb687b2
12KylaNoob <98504257+12KylaNoob@users.noreply.github.com>
2024-04-09 02:58:06 +0800

    Add files via upload

---
+++ Finals/Smart Contract/hello-world/hardhat.config.js
@@ -0,0 +1,22 @@
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.7.3",
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url:process.env.API_URL,
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
